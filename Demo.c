5f9f6d6f1d9e87f306d06ef13091709594f6146e
Yasir Ahmad <yasir.ahmad0810@gmail.com>
2022-09-27 12:45:16 +0530

    1st intro program to c

--- Demo.c
+++ Demo.c
@@ -1,5 +1,6 @@
 #include <stdio.h> //header file @ start of program
  int main(){ //main function
+  
      int a,b,c,z; //declaration
      
      printf("Enter values for a,b: "); //initialization
@@ -8,6 +9,7 @@
      z = a+b+c; //operation // c has garbage value
      
      printf("%d",z); //output
+  
      return 0; //return 
      //end of program
  }
