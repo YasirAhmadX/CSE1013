b4733679212b9f0d8f6d1d4b3e8667754063ef16
Yasir Ahmad <yasir.ahmad0810@gmail.com>
2022-10-01 13:37:24 +0530

    student details
    WAP to display student details

---
+++ practice1/Q1.c
@@ -0,0 +1,35 @@
+//WAP to take student info as input and display
+#include <stdio.h>
+
+int main() {
+    int age;
+    float height,weight;
+    char name[20],RegNo[9],Gender[1],Address[50],MobileNo[10];
+    printf("Enter name of the student: ");
+    scanf("%s",name);
+    printf("Enter RegNo: ");
+    scanf("%s",RegNo);
+    printf("Enter Age: ");
+    scanf("%d",&age);
+    printf("Enter Gender {M,F,L,G,B,T,Q}: ");
+    scanf("%s",Gender);
+    printf("Enter height(cms): ");
+    scanf("%f",&height);
+    printf("Enter weight(kgs): ");
+    scanf("%f",&weight);
+    printf("Enter Mobile No: ");
+    scanf("%s",MobileNo);
+    printf("Enter address: ");
+    scanf("%s",Address);
+    printf("-------------------\n");
+    printf("NAME: %s\n",name);
+    printf("REGISTRATION NUMBER: %s\n",RegNo);
+    printf("GENDER: %s\n",Gender);
+    printf("AGE: %d\n",age);
+    printf("HEIGHT: %.2f\n",height);
+    printf("WEIGHT: %.2f\n",weight);
+    printf("CONTACT NUMBER: %s\n",MobileNo);
+    printf("ADDRESS: %s",Address);
+
+    return 0;
+}
