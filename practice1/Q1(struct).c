d8f64609ebffcffec97780629002cdc6eee7c83a
Yasir Ahmad <yasir.ahmad0810@gmail.com>
2022-10-01 16:11:26 +0530

    Q1(struct)
    Q1 done with structures

---
+++ practice1/Q1(struct).c
@@ -0,0 +1,49 @@
+//Q1 with structures
+//WAP a prgram to input n number of student details and display it back
+#include <stdio.h>
+struct stud_details{
+    int age;
+    float height,weight;
+    char name[20],RegNo[9],Gender[1],Address[50],MobileNo[10];
+};
+
+int main() {
+    int n;
+    printf("Enter number of students: ");
+    scanf("%d",&n);
+    struct stud_details s[n];
+    for(int i=0;i<n;i++){
+        printf("-------------------\n");
+        printf("\nEnter name of the student %d: ",i+1);
+        scanf("%s",s[i].name);
+        printf("Enter RegNo: ");
+        scanf("%s",s[i].RegNo);
+        printf("Enter Age: ");
+        scanf("%d",&s[i].age);
+        printf("Enter Gender {M,F,L,G,B,T,Q}: ");
+        scanf("%s",s[i].Gender);
+        printf("Enter height(cms): ");
+        scanf("%f",&s[i].height);
+        printf("Enter weight(kgs): ");
+        scanf("%f",&s[i].weight);
+        printf("Enter Mobile No: ");
+        scanf("%s",s[i].MobileNo);
+        printf("Enter address: ");
+        scanf("%s",s[i].Address);
+    }
+    
+    for(int i=0;i<n;i++){
+        printf("-------------------\n");
+        printf("NAME: %s\n",s[i].name);
+        printf("REGISTRATION NUMBER: %s\n",s[i].RegNo);
+        printf("GENDER: %s\n",s[i].Gender);
+        printf("AGE: %d\n",s[i].age);
+        printf("HEIGHT: %.2f\n",s[i].height);
+        printf("WEIGHT: %.2f\n",s[i].weight);
+        printf("CONTACT NUMBER: %s\n",s[i].MobileNo);
+        printf("ADDRESS: %s\n",s[i].Address);
+
+    }
+
+    return 0;
+}
