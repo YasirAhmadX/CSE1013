a054266ee4156173e42619761008be04648ddecb
Yasir Ahmad <yasir.ahmad0810@gmail.com>
2022-10-11 12:49:52 +0530

    n odd natural numbers
    WAP to output n dd natural numbers

---
+++ practice2/4a.c
@@ -0,0 +1,15 @@
+//odd no. upto n
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int i=1;
+    while (i<=n){
+        printf("%d\n",i);
+        i+=2;
+    }
+
+    return 0;
+}
