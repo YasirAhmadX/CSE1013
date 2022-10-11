1e1c28fa7ab0266b88dbc32e99d94aa78c95d335
Yasir Ahmad <yasir.ahmad0810@gmail.com>
2022-10-11 12:51:15 +0530

    even natural numbers upto n
    WAP to output even natural numbers upto n

---
+++ practice2/4b.c
@@ -0,0 +1,15 @@
+//even no. upto n
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int i=2;
+    while (i<=n){
+        printf("%d\n",i);
+        i+=2;
+    }
+
+    return 0;
+}
