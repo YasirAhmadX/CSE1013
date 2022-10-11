a6d4a553ea6eea7335bf6bc332d547331f84ecbd
Yasir Ahmad <yasir.ahmad0810@gmail.com>
2022-10-11 12:47:06 +0530

    voting eligiblity
    Get age and check voting eligibility.

---
+++ practice2/2.c
@@ -0,0 +1,11 @@
+//voting eligiblity
+#include <stdio.h>
+
+int main()
+{
+    int age;
+    scanf("%d",&age);
+    printf((age<18)?"Not eligilbe for voting":"You can cast your vote!");
+
+    return 0;
+}
