ca054d244dec709cc54af01beda52b7bd96a844e
Yasir Ahmad <yasir.ahmad0810@gmail.com>
2022-10-11 12:48:26 +0530

    n natural no.
    WAP to display n natural numbers

---
+++ practice2/3.c
@@ -0,0 +1,15 @@
+//n natural numbers
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int i=1;
+    while (i<=n){
+        printf("%d\n",i);
+        i++;
+    }
+
+    return 0;
+}
