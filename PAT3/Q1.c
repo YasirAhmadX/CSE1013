ae1432ea4bb262157749a547d6285e19d21e42c2
Yasir Ahmad <yasir.ahmad0810@gmail.com>
2023-01-22 18:37:41 +0530

    duplicate arrays

---
+++ PAT3/Q1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+	int n;
+	scanf("%d",&n);
+	int i=0,sum=0;
+	for(i=2;i<=n;i++){
+		int j,p=1;
+		for(j=2;j<=i/2;j++){
+			if(i%j==0){
+				p=0;
+				break;
+			}
+		}
+		if(p==1){
+			//printf("%d",i);
+			sum+=i;
+		}
+	}
+	printf("%d",sum);
+	return 0;
+}
